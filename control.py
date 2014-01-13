b5a2079bee3dfec69e37cf9bf5f3578e1120e6c0
Owain Jones <tehdoomcat@gmail.com>
2014-01-13 00:55:03 +0000

    fuzzy matching and text-mode

--- control.py
+++ control.py
@@ -19,7 +19,9 @@ from __future__ import print_function
 
 import os
 import json
+import time
 import locale
+import difflib
 import argparse
 import subprocess
 import http.client
@@ -71,20 +73,63 @@ def record(language=LANGUAGE, duration=DURATION, rate=RATE, path=RECORD_PATH,
     return response_json
 
 
+def fuzzysubset(parent, child, word_threshold=0.5):
+    """Using levenshtein distances, check whether a child set (a set of
+       strings) is contained within a parent set.
+       Similarity ratios are calculated per word. If all of the words
+       in the child set have ratios over word_threshold to some words
+       in the parent set, this returns a dict containing the
+       child_word -> parent_word mapping.
+       If all the words in child are under word_threshold,
+       this returns None, implying that child is not a subset of parent.
+    """
+    result = dict()
+    for word_1 in child:
+        for word_2 in parent:
+            ratio = difflib.SequenceMatcher(None, word_1, word_2).ratio()
+            if ratio >= word_threshold:
+                if word_2 not in result or result[word_2][0] < ratio:
+                    result[word_2] = (ratio, word_1)
+    if len(result) == 0:
+        return None
+    return result
+
+
 def respond(config, utterance, verbose=False):
     if 'commands' not in config:
         return
     utterance_orig = utterance.lower()
     utterance = set(utterance_orig.split(' '))
+    cmd_scores = dict()
     for words, command in config['commands'].items():
         words_orig = words.lower()
         words = set(words_orig.split(' '))
-        if words.issubset(utterance):
-            hypothesis = utterance_orig.replace(words_orig, '', 1)
-            cmd = command.format(string=hypothesis, pid=str(os.getpid()))
-            if cmd.endswith('&') or cmd.endswith('exit') or cmd.endswith(';'):
-                return cmd
-            return cmd + ' &'
+        #if words.issubset(utterance):
+        fuzzy = fuzzysubset(utterance, words)
+        if fuzzy is not None:
+            score = sum([fuzzy[x][0] for x in fuzzy])
+            cmd_scores[score] = (command, fuzzy)
+            #cmd_scores[command] = (sum([fuzzy[x][0] for x in fuzzy]), fuzzy)
+        else:
+            cmd_scores[0] = (command, None)
+        #if fuzzy is not None:
+        #    hypothesis = utterance_orig.replace(words_orig, '', 1)
+        #    cmd = command.format(string=hypothesis, pid=str(os.getpid()))
+        #    if cmd.endswith('&') or cmd.endswith('exit') or cmd.endswith(';'):
+        #        return cmd
+        #    return cmd + ' &'
+    best = max(cmd_scores)
+    command = cmd_scores[best][0]
+    if best < 1.0:
+        return
+    words = [key for key, value in config['commands'].items()
+             if value == cmd_scores[best][0]][0]
+    words_orig = words.lower()
+    hypothesis = utterance_orig.replace(words_orig, '', 1)
+    cmd = command.format(string=hypothesis, pid=str(os.getpid()))
+    if cmd.endswith('&') or cmd.endswith('exit') or cmd.endswith(';'):
+        return cmd
+    return cmd + ' &'
 
 
 def main():
@@ -104,19 +149,27 @@ def main():
     parser.add_argument('-r', '--rate', type=int, default=RATE,
                         help='Sample rate')
     parser.add_argument('-v', '--verbose', action='store_true', default=False)
+    parser.add_argument('-t', '--text-mode', action='store_true',
+                        default=False)
     args = parser.parse_args()
     config = json.load(open(args.config, 'r'))
     while True:
-        last_sample = listen.listen_for_voice()
-        response = record(duration=args.duration, rate=args.rate,
-                          verbose=args.verbose, last_sample=last_sample)
+        if args.text_mode:
+            response = {'hypotheses': [{'utterance': input('> ')}]}
+        else:
+            last_sample = listen.listen_for_voice()
+            response = record(duration=args.duration, rate=args.rate,
+                              verbose=args.verbose, last_sample=last_sample)
         if response is not None and 'hypotheses' in response:
             for hypothesis in response['hypotheses']:
-                print("I heard:", hypothesis['utterance'])
+                if not args.text_mode:
+                    print("I heard:", hypothesis['utterance'])
                 command = respond(config, hypothesis['utterance'],
                                   verbose=args.verbose)
                 if command:
                     subprocess.call(command, shell=True)
+                    time.sleep(0.2)
+                    print()
 
 
 if __name__ == '__main__':
